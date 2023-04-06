9b3a25fb603eb2990f5833d1bc65e9dc492a1926
Tessak8 <tessakuil18@gmail.com>
2023-04-06 17:13:30 +0200

    update

--- javascript/javascriptwebsite.js
+++ javascript/javascriptwebsite.js
@@ -276,6 +276,7 @@ const marker5 = new maplibregl.Marker({
 
 //Leaflet kaart//
 
+
 const leafLet = L.map('leafletmap2').setView([41.902782, 12.496366], 5);
 var Stamen_TonerLite = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
